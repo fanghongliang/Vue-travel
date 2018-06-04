8a0604a3451b57e0df579f44dc9b4ed058aacdaa
fanghongliang <18292656094@163.com>
2018-06-04 21:28:47 +0800

    project init

---
+++ src/pages/Home/home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		hello world
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"home"
+	}
+</script>
+
+<style>
+	
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/pages/Home/home.vue'   
+  
+   
+//@指src目录
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes:[
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component:home
     }
   ]
 })
