13a97429bd51e1d37a78be8f8a42c273d6cc87d0
fanghongliang <18292656094@163.com>
2018-06-08 16:19:45 +0800

    city finish

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,12 +2,13 @@
 	<div>
 		<CityHeader></CityHeader>
 		<CitySearch></CitySearch>
-		<CityList></CityList>
-		<CityAlphabet></CityAlphabet>
+		<CityList :cities="cities" :hot="hotCities"></CityList>
+		<CityAlphabet :cities="cities"></CityAlphabet>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios'
 	import CityHeader from './components/Header.vue'
 	import CitySearch from './components/Search.vue'
 	import CityList from './components/List.vue'
@@ -19,6 +20,29 @@
 			CitySearch,
 			CityList,
 			CityAlphabet
+		},
+		data () {
+			return {
+				cities: {},
+				hotCities: []
+			}
+		},
+		methods : {
+			getCityInfo () {
+				axios.get('api/city.json')
+					.then(this.handleGetCityInfoSucc)
+			},
+			handleGetCityInfoSucc (res) {
+				res = res.data
+				if(res.data && res.ret) {
+					const data = res.data
+					this.cities =data.cities
+					this.hotCities = data.hotCities
+				}  
+			}
+		},
+		mounted () {
+			this.getCityInfo()
 		}
 	}
 </script>
